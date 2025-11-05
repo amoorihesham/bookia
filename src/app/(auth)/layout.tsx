8cc24f3714b238687da73afe547debc44b2b3e55
Amr <msam.team20@gmail.com>
2025-11-05 23:36:14 +0200

    temp - solve [[...sign-in]] with cache components

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,5 @@
-import { ReactNode, Suspense } from 'react';
+import { ReactNode } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-  return (
-    <main className='h-screen flex items-center justify-center'>
-      <Suspense fallback={<p>loading...</p>}>{children}</Suspense>
-    </main>
-  );
+  return <main className='h-screen flex items-center justify-center'>{children}</main>;
 }
--- src/features/events/actions/query.ts
+++ src/features/events/actions/query.ts
@@ -1,10 +1,10 @@
-import { cacheLife, cacheTag } from 'next/cache';
+// import { cacheLife, cacheTag } from 'next/cache';
 import eventsRepository from '../db/events.repo';
 
 export const GetAllEventsAction = async () => {
-  'use cache';
-  cacheLife('seconds');
-  cacheTag('events');
+  // 'use cache';
+  // cacheLife('seconds');
+  // cacheTag('events');
 
   return eventsRepository.findAllEvents();
 };
