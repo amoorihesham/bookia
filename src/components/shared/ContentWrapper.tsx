ca9ca177090e36a6541c4482e53d2f88cb10bbe6
amoorihesham <amrheshammacc@gmail.com>
2026-02-04 15:46:49 +0200

    fix container

--- src/components/shared/ContentWrapper.tsx
+++ src/components/shared/ContentWrapper.tsx
@@ -2,5 +2,5 @@ import { cn } from '@/lib/utils';
 import { ReactNode } from 'react';
 
 export const ContentWrapper = ({ children, className }: { children: ReactNode; className?: string }) => {
-  return <main className={cn('mx-auto max-w-[1440px] px-4 lg:px-0', className)}>{children}</main>;
+  return <main className={cn('mx-auto max-w-[1440px] px-4 lg:px-6', className)}>{children}</main>;
 };
